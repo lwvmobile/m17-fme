6c7a6069728216f6ccfc9beb0e0dea97fb0c47e8
lwvmobile <lwvmobile@gmail.com>
2024-05-14 16:41:25 -0400

    Fix Pulse Audio Err Condition and Strings;

--- src/pulse.c
+++ src/pulse.c
@@ -26,9 +26,12 @@ void open_pulse_audio_input (Super * super)
   }
   super->pa.pa_input_device = pa_simple_new(NULL, app_name, PA_STREAM_RECORD, NULL, session_name, &super->pa.input, NULL, &super->pa.inputlt, &err);
   super->pa.pa_input_is_open = 1;
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
+    #ifdef __CYGWIN__
+    fprintf (stderr, "Please make sure the Pulse Audio Server Backend is running first.");
+    #endif
     exit(0);
   }
 }
@@ -38,9 +41,12 @@ void open_pulse_audio_output_rf (Super * super)
   sprintf (session_name, "RF Output %04X", super->opts.random_number);
   super->pa.pa_output_device_rf = pa_simple_new(NULL, "M17-FME3", PA_STREAM_PLAYBACK, NULL, session_name, &super->pa.output_rf, NULL, NULL, &err);
   super->pa.pa_output_rf_is_open = 1;
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
+    #ifdef __CYGWIN__
+    fprintf (stderr, "Please make sure the Pulse Audio Server Backend is running first.");
+    #endif
     exit(0);
   }
 }
@@ -50,9 +56,12 @@ void open_pulse_audio_output_vx (Super * super)
   sprintf (session_name, "Voice Output %04X", super->opts.random_number);
   super->pa.pa_output_device_vx = pa_simple_new(NULL, "M17-FME4", PA_STREAM_PLAYBACK, NULL, session_name, &super->pa.output_vx, NULL, NULL, &err);
   super->pa.pa_output_vx_is_open = 1;
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
+    #ifdef __CYGWIN__
+    fprintf (stderr, "Please make sure the Pulse Audio Server Backend is running first.");
+    #endif
     exit(0);
   }
 }
@@ -82,10 +91,10 @@ short pa_input_read (Super * super)
 {
   short sample = 0;
   pa_simple_read(super->pa.pa_input_device, &sample, 2, &err);
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
-    // exit(0); //unsure if we want to exit here, may report underrun or similar issues, unknown
+    // exit(0); //pretty sure the above, if error, will just core dump before we can look anyways
   }
 
   return sample;
@@ -94,20 +103,20 @@ short pa_input_read (Super * super)
 void pulse_audio_output_rf(Super * super, short * out, size_t nsam)
 {
   pa_simple_write(super->pa.pa_output_device_rf, out, nsam*2, &err);
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
-    // exit(0);
+    // exit(0); //pretty sure the condition on this is that it blocks until discharged
   }
 }
 
 void pulse_audio_output_vx(Super * super, short * out, size_t nsam)
 {
   pa_simple_write(super->pa.pa_output_device_vx, out, nsam*2, &err);
-  if (err < 0)
+  if (err != 0)
   {
     fprintf (stderr, "%s", pa_strerror(err));
-    // exit(0);
+    // exit(0); //pretty sure the condition on this is that it blocks until discharged
   }
 }
 
